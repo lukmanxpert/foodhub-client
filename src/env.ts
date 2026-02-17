b82aa6af7fe7d5bdbabb4887a6700ba275b0a634
lukmanxpert <lukmanmiah2004@gmail.com>
2026-02-17 20:50:24 +0600

    added env validation

---
+++ src/env.ts
@@ -0,0 +1,34 @@
+// src/env.mjs
+import { createEnv } from "@t3-oss/env-nextjs"; // or core package
+import { z } from "zod";
+
+export const env = createEnv({
+  /*
+   * Serverside Environment variables, not available on the client.
+   * Will throw if you access these variables on the client.
+   */
+  server: {
+    APP_URL: z.url(),
+    SERVER_URL: z.url(),
+    SERVER_AUTH_URL: z.url(),
+  },
+  /*
+   * Environment variables available on the client (and server).
+   *
+   * 💡 You'll get type errors if these are not prefixed with NEXT_PUBLIC_.
+   */
+  //   client: {
+  //     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
+  //   },
+  /*
+   * Due to how Next.js bundles environment variables on Edge and Client,
+   * we need to manually destructure them to make sure all are included in bundle.
+   *
+   * 💡 You'll get type errors if not all variables from `server` & `client` are included here.
+   */
+  runtimeEnv: {
+    APP_URL: process.env.APP_URL,
+    SERVER_URL: process.env.SERVER_URL,
+    SERVER_AUTH_URL: process.env.SERVER_AUTH_URL,
+  },
+});
